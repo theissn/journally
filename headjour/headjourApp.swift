f00d0b3fc325c18980abf2fc5eaaa0c3f396a2ce
theissn <5305773+theissn@users.noreply.github.com>
2023-01-22 10:47:25 +0000

    Initial Commit

---
+++ headjour/headjourApp.swift
@@ -0,0 +1,17 @@
+//
+//  headjourApp.swift
+//  headjour
+//
+//  Created by Theis on 22/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct headjourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
